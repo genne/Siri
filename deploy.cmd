26f4b6001e4d1fefac5e02db0a601eecd658466b
Christian Genne <christian@jointrine.com>
2017-08-24 22:15:37 +0200

    Deploy script update

--- deploy.cmd
+++ deploy.cmd
@@ -102,6 +102,10 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
+
+  call :ExecuteCmd !NPM_CMD! run build-aurelia -- --env prod
+  IF !ERRORLEVEL! NEQ 0 goto error
+  
   popd
 )
 
